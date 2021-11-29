9a74ec9057d4a09825bfad39c3ba9f7076260651
Pranav-Bobde <bobdep31@gmail.com>
2021-11-29 23:31:36 +0530

    created PageTodo

---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <TodoTask />
+  </q-page>
+</template>
+
+<script>
+import TodoTask from 'components/TodoTask.vue'
+export default {
+  name: 'PageTodo',
+  components:{
+    TodoTask
+  }
+}
+</script>
